b377428bdad9b5757ee4e2feb5c0479f09bf8e05
mattholl <matthew.hollings@gmail.com>
2012-06-15 20:21:23 +0100

    rotation working

--- js/draw_cylinder.js
+++ js/draw_cylinder.js
@@ -16,6 +16,11 @@ function setup() {
 	mesh.rotation.x = mesh.rotation.x += Math.PI/2;
 
 	draw();
+
+	//
+	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+	//document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	//document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 }
 
 
@@ -24,13 +29,15 @@ var draw = function() {
 	setTimeout(function() {
 		requestAnimationFrame(draw);
 		
-		window.addEventListener('resize', setup, false);
+		
 
 		//mesh.rotation.y = mesh.rotation.y += ( targetRotation - mesh.rotation.y ) * 0.05;
-		
-		mesh.scale.x = mesh.scale.x += 0.5;
-		mesh.scale.z = mesh.scale.z += 0.5;
+		mesh.rotation.x = mesh.rotation.x += ( targetRotation - mesh.rotation.x ) * 0.05;
+		//mesh.scale.x = mesh.scale.x += 0.5;
+		//mesh.scale.z = mesh.scale.z += 0.5;
 	//	mesh.scale.y = mesh.scale.y -= 0.1;
+
+		window.addEventListener('resize', setup, false);
 
 		renderer.render( scene, camera );
 
@@ -38,4 +45,35 @@ var draw = function() {
 }
 
 window.onload = setup;
+
+function onDocumentMouseDown( event ) {
+console.log(event);
+		event.preventDefault();
+
+		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+		document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+
+		mouseXOnMouseDown = event.clientX - windowHalfX;
+		targetRotationOnMouseDown = targetRotation;
+	}
+
+	function onDocumentMouseMove( event ) {
+		mouseX = event.clientX - windowHalfX;
+		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+	}
+
+	function onDocumentMouseUp( event ) {
+
+		document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+		document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+		document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+	}
+
+	function onDocumentMouseOut( event ) {
+
+		document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+		document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+		document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+	}
 
--- js/three_setup.js
+++ js/three_setup.js
@@ -1,7 +1,10 @@
 var camera,
 	scene;
 	windowHalfX = window.innerWidth / 2,
-	windowHalfY = window.innerHeight / 2;
+	windowHalfY = window.innerHeight / 2,
+
+	targetRotation = 0,
+	targetRotationOnMouseDown = 0;
 
 
 (function () {
@@ -18,41 +21,9 @@ var camera,
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
 	document.body.appendChild( renderer.domElement );
-
+	//
 	//console.log(THREE);
 
 }());
 
 
-// function onDocumentMouseDown( event ) {
-
-// 		event.preventDefault();
-
-// 		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-// 		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-// 		document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-
-// 		mouseXOnMouseDown = event.clientX - windowHalfX;
-// 		targetRotationOnMouseDown = targetRotation;
-// 	}
-
-// 	function onDocumentMouseMove( event ) {
-
-// 		mouseX = event.clientX - windowHalfX;
-
-// 		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-// 	}
-
-// 	function onDocumentMouseUp( event ) {
-
-// 		document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-// 		document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-// 		document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-// 	}
-
-// 	function onDocumentMouseOut( event ) {
-
-// 		document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-// 		document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-// 		document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-// 	}
