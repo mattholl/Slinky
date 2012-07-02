2abb0ece2caea4854af80c946a4339a72d522c43
mattholl <matthew.hollings@gmail.com>
2012-07-02 20:08:46 +0100

    create beats on slinky object

--- public/js/Slinky.js
+++ public/js/Slinky.js
@@ -36,6 +36,120 @@ Slinky.prototype.createBeat = function() {
 };
 
 
+Slinky.prototype.createLowResponders = function() {
+    //roughly 0 - 2000Hz
+    var startY = -300,
+        yInc = 0;
+        freqLow = 0,
+        freqHigh = 2;
+
+
+    for(var i = 0; i < 15; i++) {
+
+        (function(i) {
+            
+            responders[i] = new Responder(startY + yInc*10);
+
+            beats[i] = dancer.createBeat({
+                frequency : [freqLow, freqHigh],
+                threshold : 0.08,
+                onBeat : function() {
+                    //start increasing the scale transform on the Responder object for this beat frequency
+                    responders[i].doScale = true;
+                },
+                offBeat : function() {
+                    //stop increasing scale, allow damping to take effect
+                    responders[i].doScale = false;
+                }
+            });
+
+            beats[i].on();
+
+        }(i));
+        
+        freqLow += 3;
+        freqHigh += 3;
+        yInc += 1;
+    }
+};
+
+
+Slinky.prototype.createMidResponders = function() {
+    //to centre to column around zero and allow setting freq bins other than incremnting with i
+    var startY = -170,
+        yInc = 0;
+        freqLow = 45,
+        freqHigh = 48;
+
+
+    for(var i = 15; i < 30; i++) {
+
+        (function(i) {
+            
+            responders[i] = new Responder(startY + yInc*10);
+
+            beats[i] = dancer.createBeat({
+                frequency : [freqLow, freqHigh],
+                threshold : 0.015,
+                onBeat : function() {
+                    //start increasing the scale transform on the Responder object for this beat frequency
+                    responders[i].doScale = true;
+                    
+                },
+                offBeat : function() {
+                    //stop increasing scale, allow damping to take effect
+                    responders[i].doScale = false;
+                }
+            });
+
+            beats[i].on();
+
+        }(i));
+        
+        freqLow += 5;
+        freqHigh += 5;
+        yInc += 1;
+    }
+};
+
+Slinky.prototype.createHighResponders = function() {
+    //to centre to column around zero and allow setting freq bins other than incremnting with i
+    var startY = -20,
+        yInc = 0;
+        freqLow = 120,
+        freqHigh = 122;
+
+    for(var i = 30; i < 45; i++) {
+
+        (function(i) {
+            
+            responders[i] = new Responder(startY + yInc*10);
+
+            beats[i] = dancer.createBeat({
+                frequency : [freqLow, freqHigh],
+                threshold : 0.009,
+                onBeat : function() {
+                    //start increasing the scale transform on the Responder object for this beat frequency
+                    responders[i].doScale = true;
+                    
+                },
+                offBeat : function() {
+                    //stop increasing scale, allow damping to take effect
+                    responders[i].doScale = false;
+                }
+            });
+
+            beats[i].on();
+
+        }(i));
+        
+        freqLow += 3;
+        freqHigh += 3;
+        yInc += 1;
+    }
+};
+
+
 window.onload = function() {
     window.app = new Slinky();
     //bind to submit
