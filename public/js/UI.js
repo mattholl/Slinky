8df27b4d5747f7844bb2ebdc7f2df705afe0af2e
mattholl <matthew.hollings@gmail.com>
2012-07-08 13:08:18 +0100

    ui dropdown controls

--- public/js/UI.js
+++ public/js/UI.js
@@ -26,7 +26,7 @@ var UI = function(player, track) {
     // })
 	
 
-    $('header').on('click', '.pulldown-triangle', this.toggleHeader);
+    $('header').on('click', '#ui-pulldown', this.toggleHeader);
 
     // document.getElementById('track-form').addEventListener('submit', function(e) {
         
@@ -57,13 +57,49 @@ UI.prototype.toggleHeader = function() {
 	//easeInExpo
 	//http://matthewlein.com/ceaser/
 
-	var elHeader = $('header');
+	var elHeader = $('header'),
+		elDropDownCircle = elHeader.find('#dropdown-icon');
 
 	if(elHeader.hasClass('open')) {
+		//move header element up to close
 		$(elHeader).transition({ y: '-100px' }, 200, 'snap');
+		//change shape of pulldown button
+		//$(elHeader).find('#ui-pulldown').toggleClass('pulldown-triangle pulldown-rectangle');
+		
+		elDropDownCircle.toggleClass('ui-closed ui-open');
+
+		$('#dropdown-cross').css('display', 'none');
+
+		$(elHeader).find('#ui-pulldown').css({
+			'border-top' : '30px solid #4d4d4d',
+			'border-left' : '30px solid transparent',
+			'border-right' : '30px solid transparent',
+			'width' : '0',
+			'height' : '0',
+			'background' : 'none'
+		});
 
 	} else if(elHeader.hasClass('closed')) {
+		//move header element down to open
 		$(elHeader).transition({ y: '0' }, 300, 'cubic-bezier(0.460, 0.045, 0.750, 0.150)' );
+		//change shape of pulldown button
+		//$(elHeader).find('#ui-pulldown').toggleClass('pulldown-triangle pulldown-rectangle');
+		
+		elDropDownCircle.toggleClass('ui-closed ui-open');
+		
+		//set dropdown-cross to display none
+		$('#dropdown-cross').css('display', 'block');
+
+
+		//problem with position being reset for icon if a css class is toggled so change these values here
+		$(elHeader).find('#ui-pulldown').css({
+			'border-top' : 'none',
+			'border-left' : 'none',
+			'border-right' : 'none',
+			'width' : '60px',
+			'height' : '30px',
+			'background' : '#4d4d4d'
+		});
 
 	} else {
 		return;
