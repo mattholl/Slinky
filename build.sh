f539e6f0b7fd859cb56fffc964b3a40134983de9
mattholl <matthew.hollings@gmail.com>
2012-07-17 22:53:26 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# cat files into one js/build/app.js
+# run through lint
+# minify app.min.js
+
+cat public/js/jquery/jquery-1.7.2.min.js \
+	public/js/jquery/transparency.min.js \
+	public/js/jquery/jquery.transit.js \
+	public/js/Responder.js \
+	public/js/Three.js \
+	public/js/Stats.js \
+	public/js/cmd.js \
+	public/js/UI.js \
+	public/js/dancer.js \
+	public/js/Slinky.js \
+	public/js/Player.js \
+>public/js/build/app.js
+
+uglifyjs --overwrite public/js/build/app.js
