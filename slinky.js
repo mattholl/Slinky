7dd5877b1db190985ddfab15f5008798cb40a90e
Matthew Hollings <matthew.hollings@gmail.com>
2015-06-28 19:42:57 +0100

    get the url for proxy

--- slinky.js
+++ slinky.js
@@ -14,7 +14,11 @@ app.use(compression());
 
 var router = express.Router();
 router.use('/proxy', function(req, res, next) {
-    console.log(req);
+
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    console.log(query);
     next();
 });
 
