b8e300b80e200b33bf6a9d6ae97aaf7211b54ac8
Matthew Hollings <matthew.hollings@gmail.com>
2015-06-30 21:21:01 +0100

    :musical_note: Tidy the server code :musical_note:

--- app.js
+++ app.js
@@ -44,87 +44,15 @@ router.use('/proxy', function(req, res, next) {
 
             // Do the media request and pipe back to the client
             var requestForMedia = http.get(scMedia, function(responseFromMedia) {
-                // console.log(responseFromMedia);
                 responseFromMedia.pipe(res);
             });
-
-            // requestForMedia.on('error', function(error) {
-            //     // res.writeHead(404);
-            //     // res.end();
-            //     next();
-            // });
-
-        } else {
-            // res.writeHead(404);
-            // res.end();
-            // next();
         }
     });
 
-    // requestToSC.on('error', function(error) {
-    //     res.writeHead(404);
-    //     res.end();
-    // });
-
-    // next();
 });
 
 app.use(router);
 app.use(express.static(__dirname + '/public'));
-
-// var app = http.createServer(function(requestToNode, responseToClient) {
-
-//     var parsed = url.parse(requestToNode.url, true);
-
-//     console.log(parsed);
-
-//     if(parsed.pathname == '/proxy') {
-//         var scURL = parsed.query.url;
-//         var parsedSCURL = url.parse(scURL);
-
-//         var requestOptions = {
-//             host : parsedSCURL.host, //'api.soundcloud.com'
-//             port : 80,
-//             path : parsedSCURL.pathname + '.json?client_id=' + client_id //'/tracks/49593184/stream?client_id86961c923d1a04425a46ac1a4a19c675'
-//         };
-
-
-//         //get media url from sc
-//         var requestToSC = http.get(requestOptions, function(responseFromSC) {
-//             if(responseFromSC.statusCode == 302) {
-//                 var mediaLocation = responseFromSC.headers.location;
-
-//                 //parse media url and compile options for another request
-//                 var parsedMediaURL = url.parse(mediaLocation);
-
-//                 var options = {
-//                     host : parsedMediaURL.host,
-//                     port : 80,
-//                     path : parsedMediaURL.pathname + parsedMediaURL.search
-//                 };
-
-//                 //do the media request
-//                 var requestForMedia = http.get(options, function(responseFromMedia) {
-//                     //stream it back to the client
-//                     responseFromMedia.pipe(responseToClient);
-//                 });
-
-//                 requestForMedia.on('error', function(error) {
-//                     responseToClient.writeHead(404);
-//                     responseToClient.end();
-//                 });
-//             } else {
-//                 responseToClient.writeHead(404);
-//                 responseToClient.end();
-//             }
-//         });
-
-//         requestToSC.on('error', function(error) {
-//             responseToClient.writeHead(404);
-//             responseToClient.end();
-//         });
-//     }
-// });
 
 app.listen(9001);
 console.log('slinky server on port 9001');
